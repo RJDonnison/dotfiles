0b6203e4351d6f402f4e2f1f0e669b8a9ce5b74f
RJDonnison <reujdon@gmail.com>
2025-10-07 01:55:35 +1300

    Updated README and install

---
+++ README.md
@@ -0,0 +1,33 @@
+# KDE plasma config
+
+This is my [Endeavour OS](https://endeavouros.com) with [KDE Plasma](https://kde.org/plasma-desktop/) system configs.
+
+## Installation
+
+The provided install script will install all the packages in `yay.txt`, it will also provide the option
+to install all flatpak apps in `flatpak.txt`. It will also configure the system with the theme and
+general settings.
+
+1. Clone the repo `git clone https://github.com/RJDonnison/dotfiles.git`
+2. Make `install.sh` exicutable with `chmod +x install.sh`
+3. Install system `./install.sh`
+4. Enjoy :)
+
+## Apps
+
+I am using a number of apps on my system that will install, mainly kitty for a terminal,
+lf for my file manager, [LazyVim](https://www.lazyvim.org) as my text editor, and rofi
+as a app launcher.
+
+## Theme
+
+The system theme is largely the [Mystical Blue Theme](https://github.com/juxtopposed/Mystical-Blue-Theme) with some minor changes.
+Icons are from [Yet Another Monocrom Icon Set](https://store.kde.org/p/2303161) with some custom icons for my apps that were missing them.
+The font used in the system is [JetBrainsMono](https://www.jetbrains.com/lp/mono/)
+
+## ROADMAP
+
+- [ ] icons
+- [ ] custom desktop files
+- [ ] pannel
+- [ ] kwin scripts
--- install.sh
+++ install.sh
@@ -1,20 +1,63 @@
 #!/bin/zsh
 
-echo "Installing packages..."
+BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
+COLOR_DIR="$HOME/.local/share/color-schemes"
+AURORAE_DIR="$HOME/.local/share/aurorae/themes"
+PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
+KVANTUM_DIR="$HOME/.config/Kvantum"
+FONT_DIR="$HOME/.local/share/fonts"
+
+echo "[*] Installing packages..."
 
 yay -S --needed - <yay.txt
-grep -v '^$' flatpak.txt | xargs -I {} flatpak install -y {}
 
-echo "Moving configs..."
+gum confirm "Would you like to install flatpak apps" && grep -v '^$' flatpak.txt | xargs -I {} flatpak install -y {}
+
+echo "[*] Moving configs..."
 
 CONFIGS=(ctpv kitty lazygit lf nvim rofi)
 for cfg in "${CONFIGS[@]}"; do
   [ -d "$HOME/.config/$cfg" ] && mv "$HOME/.config/$cfg" "$HOME/.config/$cfg.bak"
-  mv -p "$cfg" "$HOME/.config/$cfg"
+  cp "$BASE_DIR/$cfg" "$HOME/.config/$cfg" && echo "Configured $cfg"
 done
 
 [ -f "$HOME/.zshrc" ] && mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
-mv -p .zshrc "$HOME/.zshrc"
+cp .zshrc "$HOME/.zshrc" && echo "Configured zsh"
 
 [ -f "$HOME/.p10k.zsh" ] && mv "$HOME/.p10k.zsh" "$HOME/.p10k.zsh.bak"
-mv -p .p10k.zsh "$HOME/.p10k.zsh"
+cp .p10k.zsh "$HOME/.p10k.zsh" && echo "Configured PowerLevel10k"
+
+echo "[*] Installing Mystical-Blue (Jux) Theme..."
+
+mkdir -p "$COLOR_DIR" "$AURORAE_DIR" "$PLASMA_DIR" "$KVANTUM_DIR" "$FONT_DIR"
+
+cp "$BASE_DIR/JuxTheme.colors" "$COLOR_DIR/" && echo "[+] Installed color scheme: JuxTheme"
+tar -xzf "$BASE_DIR/JuxDeco.tar.gz" -C "$AURORAE_DIR/" && echo "[+] Installed Aurorae decoration: JuxDeco"
+tar -xzf "$BASE_DIR/JuxPlasma.tar.gz" -C "$PLASMA_DIR/" && echo "[+] Installed Plasma theme: JuxPlasma"
+
+if [ -f "$BASE_DIR/NoMansSkyJux.tar.gz" ]; then
+    tar -xzf "$BASE_DIR/NoMansSkyJux.tar.gz" -C "$KVANTUM_DIR/"
+    echo "[+] Installed Kvantum theme: NoMansSkyJux"
+fi
+
+echo "[*] Checking JetBrainsMono fonts..."
+FONT_COUNT=$(fc-list | grep -i "JetBrainsMono" | wc -l)
+
+if [ "$FONT_COUNT" -lt 20 ]; then
+    echo "[!] JetBrainsMono incomplete ($FONT_COUNT fonts found). Installing full set..."
+    LOCAL_FONT_DIR=$(find "$BASE_DIR" -maxdepth 2 -type d -name "JetBrainsMono-*")
+    if [ -n "$LOCAL_FONT_DIR" ]; then
+        echo "[*] Installing from local: $LOCAL_FONT_DIR"
+        find "$LOCAL_FONT_DIR" -type f -name "*.ttf" -exec cp {} "$FONT_DIR/" \;
+    else
+        echo "[*] Downloading JetBrainsMono from JetBrains..."
+        TMP_FONT=$(mktemp -d)
+        wget -qO "$TMP_FONT/JetBrainsMono.zip" "https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip"
+        unzip -qq "$TMP_FONT/JetBrainsMono.zip" -d "$TMP_FONT"
+        find "$TMP_FONT" -type f -name "*.ttf" -exec cp {} "$FONT_DIR/" \;
+    fi
+    fc-cache -fv > /dev/null
+    echo "[+] JetBrainsMono fonts installed"
+else
+    echo "[=] JetBrainsMono already installed ($FONT_COUNT files)"
+fi
