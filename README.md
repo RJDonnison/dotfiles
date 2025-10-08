a19c956e3a3087a64fa43d3ca21c0e796633a9cd
RJDonnison <reujdon@gmail.com>
2025-10-08 22:04:12 +1300

    IDK I think im done

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 This is my [Endeavour OS](https://endeavouros.com) with [KDE Plasma](https://kde.org/plasma-desktop/) system configs.
 
+### Apps
+
+I am using a number of apps on my system that will install, mainly kitty for a terminal,
+lf for my file manager, [LazyVim](https://www.lazyvim.org) as my text editor, and rofi
+as a app launcher.
+
+### Theme
+
+The system theme is largely the [Mystical Blue Theme](https://github.com/juxtopposed/Mystical-Blue-Theme) with some minor changes.
+Icons are from [Yet Another Monochrome Icon Set](https://store.kde.org/p/2303161) with some custom icons for apps that were missing them.
+The font used in the system is [JetBrainsMono](https://www.jetbrains.com/lp/mono/)
+
 ## Installation
 
 The provided install script will install all the packages in `yay.txt`, it will also provide the option
@@ -10,28 +22,39 @@ to install all flatpak apps in `flatpak.txt`. It will configure the system and i
 1. Clone the repo `git clone https://github.com/RJDonnison/dotfiles.git`
 2. Make `install.sh` executable with `chmod +x install.sh`
 3. Install system `./install.sh`
-4. set icons, colors, plasma style, window deco, enable better blur in settings
-5. set active kvantum theme in kvantum manager
-6. set application style as kvantum theme in settings
+
+### Post Install
+
+To fully configure the system you must apply all the themes.
+
+1. Import keybinds from `./shortcuts.kksrc` by selecting import in shortcuts section, some apps you may need to configure manually.
+2. Open Kvantum Manager and change the theme to NoMansSkyJux in Change/Delete Theme
+3. Then set the application theme to kvantum in system settings
+4. Also in system settings set icons to YAMIS, colors to JuxTheme, Plasma style to JuxPlasma, window decorations to JuxDeco
+5. Set the fonts to use JetBrainsMono Nerd Font Mono 10pt, by adjusting all fonts in system settings.
+6. Still in system settings, enable better blur under desktop effect, its recommended to apply to all window by setting force blur on all.
 7. Enjoy :)
 
-## Apps
+### Panel Installation
 
-I am using a number of apps on my system that will install, mainly kitty for a terminal,
-lf for my file manager, [LazyVim](https://www.lazyvim.org) as my text editor, and rofi
-as a app launcher.
+To setup the panel like the images you must install Panel Colorizer, SCP, and PlasMusic Toolbar. These can all be found in add or manage widgets.
 
-## Theme
+1. Add Panel Colorizer to the panel
+2. Set Panel Colorizer to hide widget
+3. Open Panel Colorizer config and set preset to RJDonnisonPanel
+4. Set widgets
 
-The system theme is largely the [Mystical Blue Theme](https://github.com/juxtopposed/Mystical-Blue-Theme) with some minor changes.
-Icons are from [Yet Another Monochrome Icon Set](https://store.kde.org/p/2303161) with some custom icons for apps that were missing them.
-The font used in the system is [JetBrainsMono](https://www.jetbrains.com/lp/mono/)
+The widgets from left to right in the images are SCP (icon com.abisource.AbiWord), spacer, Icons-Only task manager, spacer, PlasMusic Toolbar, System Tray, and Digital Clock.
 
-## ROADMAP
+## Recommended Settings
 
-- [ ] panel - instructions
-- [x] add .oh-my-zsh.sh - install
-- [x] keybinds - use export
-- [x] install desktops correct - install
-- [ ] add option to remove unused apps - install
-- [ ] add wallpaper
+- Disable splash screen
+- Set wallpaper, the images use `./images/city.gif`
+- Set Login screen to Breeze, and set custom background
+- Install tiler KWin script
+- Set screen edges to have no action
+- Set default applications
+- Add a few extra virtual desktops
+- Increase screen locking time
+
+![Image of desktop](./images/desktop.png)
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 set -e
 
@@ -13,11 +13,22 @@ ICONS_DIR="$HOME/.local/share/icons"
 
 echo "[*] Installing packages..."
 
-yay -S --needed - < "$BASE_DIR/yay.txt"
+yay -S --needed - <"$BASE_DIR/yay.txt"
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo "Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  echo "[*] Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattende
+  if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+      "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+  fi
+
+  if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/plugins}/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+      "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/plugins}/zsh-syntax-highlighting"
+  fi
+
+  echo "[+] zsh installed"d
 fi
 
 gum confirm "Would you like to install flatpak apps?" && grep -v '^$' "$BASE_DIR/flatpak.txt" | xargs -I {} flatpak install -y {}
@@ -39,16 +50,17 @@ cp -r .zshrc "$HOME/.zshrc" && echo "[+] Configured zsh"
 cp -r .p10k.zsh "$HOME/.p10k.zsh" && echo "[+] Configured PowerLevel10k"
 
 if [ -d "$BASE_DIR/desktop" ]; then
-    echo "[*] Copying desktop files..."
-    for file in "$BASE_DIR/desktop/"*; do
-        if [ -f "$file" ]; then
-            sudo cp -r "$file" "$desktop_dir/"
-            echo "[*] Copied $(basename "$file")"
-        fi
-    done
-    echo "[*] Desktop files configured."
+  echo "[*] Copying desktop files..."
+  for file in "$BASE_DIR/desktop/"*; do
+    filename=$(basename "$file")
+    if [ -f "$BASE_DIR/desktop/$filename" ]; then
+      sudo cp -r "$BASE_DIR/desktop/$filename" "$desktop_dir/"
+      echo "[*] Copied $(basename "$file")"
+    fi
+  done
+  echo "[*] Desktop files configured."
 else
-    echo "[!] Desktop source directory not found: $base_dir/desktop"
+  echo "[!] Desktop source directory not found: $base_dir/desktop"
 fi
 
 echo "[*] Installing Mystical-Blue (Jux) Theme..."
@@ -60,30 +72,30 @@ tar -xzf "$BASE_DIR/theme/JuxDeco.tar.gz" -C "$AURORAE_DIR/" && echo "[+] Instal
 tar -xzf "$BASE_DIR/theme/JuxPlasma.tar.gz" -C "$PLASMA_DIR/" && echo "[+] Installed Plasma theme: JuxPlasma"
 
 if [ -f "$BASE_DIR/theme/NoMansSkyJux.tar.gz" ]; then
-    tar -xzf "$BASE_DIR/theme/NoMansSkyJux.tar.gz" -C "$KVANTUM_DIR/"
-    echo "[+] Installed Kvantum theme: NoMansSkyJux"
+  tar -xzf "$BASE_DIR/theme/NoMansSkyJux.tar.gz" -C "$KVANTUM_DIR/"
+  echo "[+] Installed Kvantum theme: NoMansSkyJux"
 fi
 
 echo "[*] Checking JetBrainsMono fonts..."
 FONT_COUNT=$(fc-list | grep -i "JetBrainsMono" | wc -l)
 
 if [ "$FONT_COUNT" -lt 20 ]; then
-    echo "[!] JetBrainsMono incomplete ($FONT_COUNT fonts found). Installing full set..."
-    LOCAL_FONT_DIR=$(find "$BASE_DIR" -maxdepth 2 -type d -name "JetBrainsMono-*")
-    if [ -n "$LOCAL_FONT_DIR" ]; then
-        echo "[*] Installing from local: $LOCAL_FONT_DIR"
-        find "$LOCAL_FONT_DIR" -type f -name "*.ttf" -exec cp {} "$FONT_DIR/" \;
-    else
-        echo "[*] Downloading JetBrainsMono from JetBrains..."
-        TMP_FONT=$(mktemp -d)
-        wget -qO "$TMP_FONT/JetBrainsMono.zip" "https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip"
-        unzip -qq "$TMP_FONT/JetBrainsMono.zip" -d "$TMP_FONT"
-        find "$TMP_FONT" -type f -name "*.ttf" -exec cp {} "$FONT_DIR/" \;
-    fi
-    fc-cache -fv > /dev/null
-    echo "[+] JetBrainsMono fonts installed"
+  echo "[!] JetBrainsMono incomplete ($FONT_COUNT fonts found). Installing full set..."
+  LOCAL_FONT_DIR=$(find "$BASE_DIR" -maxdepth 2 -type d -name "JetBrainsMono-*")
+  if [ -n "$LOCAL_FONT_DIR" ]; then
+    echo "[*] Installing from local: $LOCAL_FONT_DIR"
+    find "$LOCAL_FONT_DIR" -type f -name "*.ttf" -exec cp {} "$FONT_DIR/" \;
+  else
+    echo "[*] Downloading JetBrainsMono from JetBrains..."
+    TMP_FONT=$(mktemp -d)
+    wget -qO "$TMP_FONT/JetBrainsMono.zip" "https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip"
+    unzip -qq "$TMP_FONT/JetBrainsMono.zip" -d "$TMP_FONT"
+    find "$TMP_FONT" -type f -name "*.ttf" -exec cp {} "$FONT_DIR/" \;
+  fi
+  fc-cache -fv >/dev/null
+  echo "[+] JetBrainsMono fonts installed"
 else
-    echo "[=] JetBrainsMono already installed ($FONT_COUNT files)"
+  echo "[=] JetBrainsMono already installed ($FONT_COUNT files)"
 fi
 
 echo "[*] Installing icons..."
@@ -94,7 +106,6 @@ tar -xzf "$BASE_DIR/theme/YAMIS.tar.gz" -C "$ICONS_DIR/" && echo "[+] Installed 
 
 echo "[+] Installed icons"
 
-gum confirm "Would you like to remove unneeded apps?" && yay -Rns < "$BASE_DIR/remove.txt" && yay -Rns $(yay -Qdtq)
+gum confirm "Would you like to remove unneeded apps?" && yay -Rns <"$BASE_DIR/remove.txt" && yay -Rns $(yay -Qdtq)
 
 echo "[+] System installed"
-
