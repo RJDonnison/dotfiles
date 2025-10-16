8ca932e5b63e0b2876b0a8feab79616e130cadf6
RJDonnison <reujdon@gmail.com>
2025-10-17 11:38:03 +1300

    Added nvim

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.filetype.add({ extension = { ejs = "ejs" } })
+vim.treesitter.language.register("html", "ejs")
+vim.treesitter.language.register("javascript", "ejs")
+vim.treesitter.language.register("embedded_template", "ejs")
